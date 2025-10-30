8ba68f2d996e6280d63dd37fe2a00a4b7d9dc98c
callum <mcmahon.callum@gmail.com>
2025-10-30 23:35:39 +0000

    shell.nix cleanup

--- shell.nix
+++ shell.nix
@@ -1,10 +1,4 @@
-{ pkgs ? import <nixpkgs> {} }: let
-  hugo-theme = builtins.fetchTarball {
-    name = "hugo-theme-m10c";
-    url = "https://github.com/vaga/hugo-theme-m10c/archive/8295ee808a8166a7302b781895f018d9cba20157.tar.gz";
-    sha256 = "12jvbikznzqjj9vjd1hiisb5lhw4hra6f0gkq1q84s0yq7axjgaw";
-  };
-in
+{ pkgs ? import <nixpkgs> {} }:
   pkgs.mkShellNoCC {
     packages = [
       pkgs.git
@@ -12,8 +6,4 @@ in
       pkgs.just
     ];
 
-    shellHook = ''
-      mkdir -p themes
-      ln -snf "${hugo-theme}" themes/m10c
-    '';
   }
